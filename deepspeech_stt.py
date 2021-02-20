09b6fd755ecebec33d4963c849f59118d1d8b6b6
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-20 16:51:30 +0530

    checking with scipy.io.wavfile

--- deepspeech_stt.py
+++ deepspeech_stt.py
@@ -12,7 +12,6 @@ import wave
 import json
 import time
 import deepspeech
-import sox
 
 from timeit import default_timer as timer
 
@@ -25,44 +24,63 @@ MODEL_PATH = "deepspeech-0.9.3-models.pbmm"
 SCORER_PATH = "deepspeech-0.9.3-models.scorer"
 audio_path = input('Enter the audio file path: ')
 
-print(sox.file_info.sample_rate(audio_path))
+import numpy as np
+from scipy.io import wavfile
+from scipy import interpolate
 
-# def convert_samplerate(audio_path, desired_sample_rate):
-#     sox_cmd = 'sox {} --type raw --bits 16 --channels 1 --rate {} --encoding signed-integer --endian little --compression 0.0 --no-dither - '.format(
-#         quote(audio_path), desired_sample_rate)
-#     try:
-#         output = subprocess.check_output(shlex.split(sox_cmd), stderr=subprocess.PIPE)
-#     except subprocess.CalledProcessError as e:
-#         raise RuntimeError('SoX returned non-zero status: {}'.format(e.stderr))
-#     except OSError as e:
-#         raise OSError(e.errno,
-#                       'SoX not found, use {}hz files or install it: {}'.format(desired_sample_rate, e.strerror))
+NEW_SAMPLERATE = 16000
+
+old_samplerate, old_audio = wavfile.read(audio_path)
+
+print(f'Original sample rate: {old_samplerate}')
+
+# if old_samplerate != NEW_SAMPLERATE:
+#     duration = old_audio.shape[0] / old_samplerate
 #
-#     return desired_sample_rate, np.frombuffer(output, np.int16)
+#     time_old  = np.linspace(0, duration, old_audio.shape[0])
+#     time_new  = np.linspace(0, duration, int(old_audio.shape[0] * NEW_SAMPLERATE / old_samplerate))
 #
+#     interpolator = interpolate.interp1d(time_old, old_audio.T)
+#     new_audio = interpolator(time_new).T
 #
-# def main(audio_path):
-#     # parser = argparse.ArgumentParser(description='Speech to text using deepspeech')
-#     # parser.add_argument('--audio', required=True, help='Path to the audio file to run (WAV format)')
-#     # args = parser.parse_args()
-#     # audio_path = args.audio
-#     # initialise model
-#     model = deepspeech.Model(MODEL_PATH)
-#     # configure scorer
-#     model.enableExternalScorer(SCORER_PATH)
-#     fin = wave.open(audio_path, 'rb')
-#     fs_orig = fin.getframerate()
-#     desired_sample_rate = model.sampleRate()
-#     fs_new, audio = convert_samplerate(audio_path, desired_sample_rate)
-#     audio_length = fin.getnframes() * (1 / fs_orig)
-#     print("audio-length {}".format(str(audio_length)))
-#     output_text = model.stt(audio)
-#     print(output_text)
-#     file_name = "{}_text_{}.txt".format(audio_path.split("/")[-1], str(time.time()))
-#     output_text_file = open(file_name, "a")
-#     output_text_file.write(output_text)
-#     output_text_file.close()
+#     wavfile.write("out.wav", NEW_SAMPLERATE, np.round(new_audio).astype(old_audio.dtype))
 #
-#
-# if __name__ == '__main__':
-#     main(audio_path)
+# # def convert_samplerate(audio_path, desired_sample_rate):
+# #     sox_cmd = 'sox {} --type raw --bits 16 --channels 1 --rate {} --encoding signed-integer --endian little --compression 0.0 --no-dither - '.format(
+# #         quote(audio_path), desired_sample_rate)
+# #     try:
+# #         output = subprocess.check_output(shlex.split(sox_cmd), stderr=subprocess.PIPE)
+# #     except subprocess.CalledProcessError as e:
+# #         raise RuntimeError('SoX returned non-zero status: {}'.format(e.stderr))
+# #     except OSError as e:
+# #         raise OSError(e.errno,
+# #                       'SoX not found, use {}hz files or install it: {}'.format(desired_sample_rate, e.strerror))
+# #
+# #     return desired_sample_rate, np.frombuffer(output, np.int16)
+# #
+# #
+# # def main(audio_path):
+# #     # parser = argparse.ArgumentParser(description='Speech to text using deepspeech')
+# #     # parser.add_argument('--audio', required=True, help='Path to the audio file to run (WAV format)')
+# #     # args = parser.parse_args()
+# #     # audio_path = args.audio
+# #     # initialise model
+# #     model = deepspeech.Model(MODEL_PATH)
+# #     # configure scorer
+# #     model.enableExternalScorer(SCORER_PATH)
+# #     fin = wave.open(audio_path, 'rb')
+# #     fs_orig = fin.getframerate()
+# #     desired_sample_rate = model.sampleRate()
+# #     fs_new, audio = convert_samplerate(audio_path, desired_sample_rate)
+# #     audio_length = fin.getnframes() * (1 / fs_orig)
+# #     print("audio-length {}".format(str(audio_length)))
+# #     output_text = model.stt(audio)
+# #     print(output_text)
+# #     file_name = "{}_text_{}.txt".format(audio_path.split("/")[-1], str(time.time()))
+# #     output_text_file = open(file_name, "a")
+# #     output_text_file.write(output_text)
+# #     output_text_file.close()
+# #
+# #
+# # if __name__ == '__main__':
+# #     main(audio_path)
