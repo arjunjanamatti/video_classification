e353842a5e2ee968746d968f86a953e53a6b7acc
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-22 12:16:26 +0530

    check performance of multiprocess for 3 videos

--- video_multi_process.py
+++ video_multi_process.py
@@ -6,6 +6,8 @@ import concurrent.futures
 import predict
 import operator
 
+model = predict.load_model('nsfw.299x299.h5')
+
 image_directory = 'C:/Users/Arjun Janamatti/PycharmProjects/jeeva_project/video_and_image_classification/uploads'
 # storing the frames from training videos
 frames_directory_name = 'C:/Users/Arjun Janamatti/PycharmProjects/jeeva_project/video_and_image_classification/frames_from_videos'
@@ -17,43 +19,43 @@ videos_list = glob(f'C:/Users/Arjun Janamatti/PycharmProjects/jeeva_project/vide
 def video_process(video):
     vidObj = cv.VideoCapture(video)
     print(f'Video: {video} ')
-    # video_file_name = (videos_list[index].split('\\')[-1]).split('.')[0]
-    # count = 0
-    # success = 1
-    # fps = vidObj.get(cv.CAP_PROP_FPS)
-    # while success:
-    #     try:
-    #         success, image = vidObj.read()
-    #         count += 1
-    #         if count%(int(fps)*2)==0:
-    #         # if count % 300 == 0:
-    #           cv.imwrite("{}/{}_frame_{}.jpg".format(frames_directory_name,video_file_name, count), image)
-    #     except:
-    #       pass
-    # result = predict.classify(model, '{}/'.format(frames_directory_name))
-    # files = glob('{}/*'.format(frames_directory_name))
-    # num_images_in_folder = len(files)
-    # for file in files:
-    #     os.remove(file)
-    # count_unsafe = 0
-    # for key in result.keys():
-    #     if (max(result[key].items(), key=operator.itemgetter(1))[0] == 'porn') or (
-    #             max(result[key].items(), key=operator.itemgetter(1))[0] == 'sexy'):
-    #         count_unsafe += 1
-    #
-    #
+    video_file_name = ((video.split("/")[-1]).split("\\")[-1]).split('.')[0]
+    count = 0
+    success = 1
+    fps = vidObj.get(cv.CAP_PROP_FPS)
+    while success:
+        try:
+            success, image = vidObj.read()
+            count += 1
+            if count%(int(fps)*2)==0:
+            # if count % 300 == 0:
+              cv.imwrite("{}/{}_frame_{}.jpg".format(frames_directory_name,video_file_name, count), image)
+        except:
+          pass
+    result = predict.classify(model, '{}/'.format(frames_directory_name))
+    files = glob('{}/*'.format(frames_directory_name))
+    num_images_in_folder = len(files)
+    for file in files:
+        os.remove(file)
+    count_unsafe = 0
+    for key in result.keys():
+        if (max(result[key].items(), key=operator.itemgetter(1))[0] == 'porn') or (
+                max(result[key].items(), key=operator.itemgetter(1))[0] == 'sexy'):
+            count_unsafe += 1
+
+
     # column_a.append(count_unsafe)
     # column_b.append(num_images_in_folder)
-    # percent_unsafe = round(count_unsafe / num_images_in_folder * 100, 2)
-    # if percent_unsafe > 50:
-    #     print(f'"UNSAFE VIDEO", since percentage of unsafe images: {percent_unsafe}%')
-    # elif (percent_unsafe > 30) & (percent_unsafe <= 50):
-    #     print(f'"ADMIN HAS TO VERIFY", since percentage of unsafe images: {percent_unsafe}%')
-    # elif (percent_unsafe > 20) & (percent_unsafe <= 30):
-    #     print(f'"ADMIN CAN VERIFY or IGNORE", since percentage of unsafe images: {percent_unsafe}%')
-    # else:
-    #     print(f'"SAFE VIDEO", since percentage of unsafe images: {percent_unsafe}%')
-    # pass
+    percent_unsafe = round(count_unsafe / num_images_in_folder * 100, 2)
+    if percent_unsafe > 50:
+        print(f'"UNSAFE VIDEO", since percentage of unsafe images: {percent_unsafe}%')
+    elif (percent_unsafe > 30) & (percent_unsafe <= 50):
+        print(f'"ADMIN HAS TO VERIFY", since percentage of unsafe images: {percent_unsafe}%')
+    elif (percent_unsafe > 20) & (percent_unsafe <= 30):
+        print(f'"ADMIN CAN VERIFY or IGNORE", since percentage of unsafe images: {percent_unsafe}%')
+    else:
+        print(f'"SAFE VIDEO", since percentage of unsafe images: {percent_unsafe}%')
+    pass
 
 def CheckConcurrent():
     start = time.perf_counter()
