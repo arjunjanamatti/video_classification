bd22f5db8c6fb5a11191a0862a0e919fc44656af
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-26 14:12:06 +0530

    changed constant rate factor to 35

--- trial_compress_video.py
+++ trial_compress_video.py
@@ -12,7 +12,7 @@ def get_codecs():
     # x = subprocess.check_output(["C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe", "-i", "sample_video.mp4",
     #                              "-vcodec", "msmpeg4v2", "-acodec", "copy", "sample_video_1.mp4"],shell=True)
     x = subprocess.check_output(["C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe", "-i", "sample_video.mp4",
-                                 "-vcodec", "libx264", "-crf", "18", "sample_video_1.mp4"],shell=True)
+                                 "-vcodec", "libx264", "-crf", "35", "sample_video_1.mp4"],shell=True)
     finish = time.perf_counter()
 
     print(f'Finished in {round(finish-start, 2)} seconds(s) ')
@@ -21,7 +21,8 @@ def get_codecs():
     #     print(e)
 
 
-# ffmpeg -i input.mp4 -vcodec libx264 -crf 18 output.mp4 -- 142mb
+# ffmpeg -i input.mp4 -vcodec libx264 -crf 28 output.mp4 -- 78.6mb [reduced file size] time taken is 500 seconds
+# ffmpeg -i input.mp4 -vcodec libx264 -crf 18 output.mp4 -- 172mb
 # ffmpeg -i input.mp4 -vcodec libx264 -crf 20 output.mp4 -- 142mb
 # ffmpeg -i input.avi -vcodec msmpeg4v2 -acodec copy output.avi
 # ffmpeg -i sample_video.mp4 -c:v libx264 -preset superfast -b:a 256k -vf -b:v 1.5M -c:a aac sample_video_1.mp4
