4ba21e60b82d49eb03dfdc6b311abbc713b1c274
arjunjanamatti <arjunjanamatti@gmail.com>
2021-02-26 13:23:35 +0530

    trying ffmpeg vcodec libx264

--- trial_compress_video.py
+++ trial_compress_video.py
@@ -4,11 +4,18 @@ import subprocess
 
 def get_codecs():
     cmd = "ffmpeg -codecs"
-    x = subprocess.check_output(["C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe", "-i", "sample_video.mp4", "sample_video_1.mp4"], shell=True)
+    # x = subprocess.check_output(["C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe", "-i", "sample_video.mp4", "sample_video_1.mp4"], shell=True)
+    # x = subprocess.check_output(["C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe", "-i", "sample_video.mp4", '-c:v','libx264',
+    #                              '-preset', 'superfast','-b:a', '256k', '-vf', '-c:a','aac', "sample_video_1.mp4"], shell=True)
+    x = subprocess.check_output(["C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe", "-i", "sample_video.mp4",
+                                 "-vcodec", "msmpeg4v2", "-acodec", "copy", "sample_video_1.mp4"],shell=True)
     print(x)
     # x = x.split(b'\n')
     # for e in x:
     #     print(e)
 
+# ffmpeg -i input.mp4 -vcodec libx264 -crf 20 output.mp4
+# ffmpeg -i input.avi -vcodec msmpeg4v2 -acodec copy output.avi
+# ffmpeg -i sample_video.mp4 -c:v libx264 -preset superfast -b:a 256k -vf -b:v 1.5M -c:a aac sample_video_1.mp4
 # ffmpeg -i <inputfilename> -s 640x480 -b:v 512k -vcodec mpeg1video -acodec copy <outputfilename>
 get_codecs()
