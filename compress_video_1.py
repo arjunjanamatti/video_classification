599edee081998542fb128399b88cd3f893d37a84
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-02 00:02:58 +0530

    update with duration, bitrate

--- compress_video_1.py
+++ compress_video_1.py
@@ -1,6 +1,8 @@
 import subprocess
 import time
 import base64
+import cv2
+from pathlib import Path
 
 ffmpeg_location = "C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe"
 
@@ -37,4 +39,15 @@ def compress_wit_base_64(video):
     pass
 
 video = 'sample.mp4'
-compress_wit_base_64(video)
+file = video
+file_size = Path(file).stat().st_size
+v = cv2.VideoCapture(file)
+fps = v.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
+frame_count = int(v.get(cv2.CAP_PROP_FRAME_COUNT))
+duration_seconds = frame_count/fps
+# duration_seconds = v.get(cv2.CAP_PROP_POS_MSEC)
+actual_bitrate = file_size / duration_seconds
+compressed_file_size = 0.1 * file_size
+assigned_bitrate = compressed_file_size / duration_seconds
+print(actual_bitrate, assigned_bitrate)
+# compress_wit_base_64(video)
