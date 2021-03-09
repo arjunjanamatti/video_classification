ccc7297761fa11f14c33d0d3d4d3a8d9b1006d4a
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-09 16:08:29 +0530

    speech to text in video class oop complete

--- classify_and_speech_to_text/1.py
+++ classify_and_speech_to_text/1.py
@@ -32,44 +32,32 @@ class speech_to_text:
         print(f'Finished in {round(finish - start, 2)} seconds(s) ')
         return output
 
+    def ProfaneWordList(self):
+        try:
+            with open(file='data.pickle', mode='rb') as file:
+                word_list_df = pickle.load(file)
+        except:
+            df = pd.read_csv('profane_word_list.txt', header=None)
+            word_list_df = [words for words in df.iloc[:, -1]]
+
+            with open('data.pickle', mode='wb') as file:
+                pickle.dump(word_list_df, file)
+
+        return word_list_df
+
+    def MainResult(self):
+        video_text = self.VideoToText()
+        video_text = video_text.decode('utf-8')
+        word_list_df = self.ProfaneWordList()
+        profane_words_list = []
+        for words in word_list_df:
+            if words in video_text:
+                profane_words_list.append(words)
+        return f'{self.video_file} has approximately {len(profane_words_list)} number of profane words and profane words in speech are {profane_words_list}!!!'
+        pass
+
     pass
 
-def VideoToText(filename):
-    ffmpeg_location = "C:/PATH_programs/ffmpeg-4.3.2-2021-02-20-full_build/bin/ffmpeg.exe"
-
-    video = filename
-    start = time.perf_counter()
-    command = [ffmpeg_location, "-i", f"{video}", "-ac", "1", "-ab", "16000","-ar", "16000","temp_output.wav"]
-    compress_video = subprocess.check_output(command, shell=True)
-
-    audio_filename = 'temp_output.wav'
-    proc = subprocess.Popen(
-        f"deepspeech --model {MODEL_PATH}  --audio " + audio_filename,
-        shell=True, stdout=subprocess.PIPE, )
-    output = proc.communicate()[0]
-    finish = time.perf_counter()
-    os.remove("temp_output.wav")
-    print(f'Finished in {round(finish - start, 2)} seconds(s) ')
-    return output
-
-try:
-    with open(file='data.pickle', mode='rb') as file:
-        word_list_df = pickle.load(file)
-except:
-    df = pd.read_csv('profane_word_list.txt', header = None)
-    print(df)
-    word_list_df = [words for words in df.iloc[:,-1]]
-    print(word_list_df)
-
-    with open('data.pickle', mode='wb') as file:
-        pickle.dump(word_list_df, file)
-
-
-filename = 'sample.mp4'
-video_text = VideoToText(filename)
-video_text = video_text.decode('utf-8')
-print(video_text)
-print(len(video_text))
-for words in word_list_df:
-    if words in video_text:
-        print(words)
+file = 'sample.mp4'
+check = speech_to_text(file)
+print(check.MainResult())
