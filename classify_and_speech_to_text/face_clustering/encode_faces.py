dffe80ce5495278639b7a8742bc485071b842fa3
arjunjanamatti <arjunjanamatti@gmail.com>
2021-03-12 14:30:16 +0530

    updated encode faces with args parser

--- classify_and_speech_to_text/face_clustering/encode_faces.py
+++ classify_and_speech_to_text/face_clustering/encode_faces.py
@@ -14,3 +14,19 @@ import cv2
 # operating system
 import os
 from constants import ENCODINGS_PATH
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True,
+	help="path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized database of facial encodings")
+ap.add_argument("-d", "--detection_method", type=str, default="cnn",
+	help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+
+# grab the paths to the input images in our dataset, then initialize
+# out data list (which we'll soon populate)
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+data = []
